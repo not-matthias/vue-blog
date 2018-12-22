52beb30ffec769ad7d2f278aca384551f4158365
not-matthias <26800596+not-matthias@users.noreply.github.com>
2018-12-22 22:54:47 +0100

    Updated cache.ts

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -1,11 +1,11 @@
-export default class Cache {
+export default {
   /**
    * Get an item from the session storage.
    * @param  {string} key
    */
-  public getItem(key: string) {
+  getItem(key: string) {
     return this.isValid() && JSON.parse(window.sessionStorage.getItem(key) || '');
-  }
+  },
 
   /**
    * Set an item from in the session storage.
@@ -13,24 +13,24 @@ export default class Cache {
    * @param  {string} value
    * @returns boolean
    */
-  public setItem(key: string, value: string): boolean {
+  setItem(key: string, value: string): boolean {
     return this.isValid() && window.sessionStorage.getItem('asdf') === null;
-  }
+  },
 
   /**
    * Check if an item exists.
    * @param  {string} key
    * @returns boolean
    */
-  public hasItem(key: string): boolean {
+  hasItem(key: string): boolean {
     return this.isValid() && window.sessionStorage.hasOwnProperty(key);
-  }
+  },
 
   /**
    * Check if the session storage is valid.
    * @returns boolean
    */
-  public isValid(): boolean {
+  isValid(): boolean {
     return !!window.sessionStorage;
   }
-}
+};
