d15e2dafa301a9a8694d428d8e3688fb1ef128d2
not-matthias <26800596+not-matthias@users.noreply.github.com>
2018-12-25 18:24:05 +0100

    Removed categories (will be implemented later)

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,7 +9,7 @@
       <v-toolbar-items>
         <v-btn flat @click="$router.push({ name: 'posts' })">Posts</v-btn>
 
-        <v-btn flat v-for="(category, key) in categories" :key="key">{{ category.name }}</v-btn>
+        <!-- <v-btn flat v-for="(category, key) in categories" :key="key">{{ category.name }}</v-btn> -->
       </v-toolbar-items>
     </v-toolbar>
   </div>
@@ -26,6 +26,9 @@ export default class Header extends Vue {
   private name: string = 'My Blog';
   private categories: ICategory[] = [];
 
+  /**
+   * Sets the variables via the config.
+   */
   private created() {
     this.name = config.title;
     this.categories = config.categories;
