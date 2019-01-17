cac3f81df1eba072022bcefc88581a3785259983
not-matthias <26800596+not-matthias@users.noreply.github.com>
2019-01-17 22:35:26 +0100

    Homepage update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,17 +8,13 @@
           <v-layout align-center>
             <v-flex text-xs-center>
               <h1
+                class="font-weight-thin"
                 :class="{
                   'display-4 pb-3' : $vuetify.breakpoint.smAndUp, 
                   'display-3 pb-1': $vuetify.breakpoint.xsOnly,
                 }"
               >Welcome</h1>
-              <h3
-                :class="{
-                  'headline' : $vuetify.breakpoint.smAndUp, 
-                  'body-2': $vuetify.breakpoint.xsOnly,
-                }"
-              >I write about different computer science topics.</h3>
+              <h3 class="font-weight-thin headline">I write about different computer science topics.</h3>
             </v-flex>
           </v-layout>
         </v-container>
