2f4fc0281d1891a48b60be96fabda8afa7d40fcc
not-matthias <26800596+not-matthias@users.noreply.github.com>
2020-01-24 20:13:22 +0100

    Ignoring the typescript

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import vuetify from './plugins/vuetify';
-
-// Import service worker
 import './registerServiceWorker';
-
-// Import stylesheets
-import 'vuetify/dist/vuetify.min.css';
-import 'material-design-icons-iconfont/dist/material-design-icons.css';
-import 'highlight.js/styles/darcula.css';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  // @ts-ignore
   vuetify,
   render: h => h(App)
 }).$mount('#app');
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,5 +1,16 @@
 import Vue from 'vue';
+// @ts-ignore
 import Vuetify from 'vuetify/lib';
+
+// Import stylesheets
+import 'vuetify/dist/vuetify.min.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
+
+declare module 'vue/types/vue' {
+    interface Vue {
+            $vuetify: any;
+    }
+}
 
 Vue.use(Vuetify);
 
